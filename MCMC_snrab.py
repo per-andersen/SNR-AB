c3b5adf19f60fb5cb0abb883f652969c53345929
Per Andersen <perandersen@dark-cosmology.dk>
2017-04-20 16:06:35 +0200

    Added data and simple import

---
+++ MCMC_snrab.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+gp13 = np.genfromtxt('GP13.txt')
+m05 = np.genfromtxt('M05.txt')
+s06 = np.genfromtxt('S06.txt')
+sm12 = np.genfromtxt('Sm12.txt') 
+
+#print gp13
+#print m05
+#print s06
+print sm12
