9c30db9314744bc77a548c8a9b334e8ed83875d4
Per Andersen <perandersen@dark-cosmology.dk>
2017-04-20 16:22:23 +0200

    Added simple plot

--- MCMC_snrab.py
+++ MCMC_snrab.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 gp13 = np.genfromtxt('GP13.txt')
 m05 = np.genfromtxt('M05.txt')
@@ -9,3 +10,12 @@ sm12 = np.genfromtxt('Sm12.txt')
 #print m05
 #print s06
 print sm12
+
+ax = plt.subplot()
+ax.set_yscale("log")
+plt.errorbar(gp13[:,0],gp13[:,1],yerr=[gp13[:,2],gp13[:,3]])
+plt.errorbar(m05[:,0],m05[:,1],yerr=[m05[:,2],m05[:,3]])
+plt.errorbar(s06[:,0],s06[:,1],yerr=2e-14)
+plt.errorbar(sm12[:,0],sm12[:,1],yerr=2e-14)
+plt.show()
+
